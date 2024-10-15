cc352c12ea4bbf052a8f925c270de7d124ebb836
DivyaE4 <divs1854@gmail.com>
2024-10-15 12:44:59 +0530

    added details

--- models.py
+++ models.py
@@ -9,6 +9,8 @@ class User(db.Model):
     username = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
     age = db.Column(db.Integer, nullable=False)
+    blood_type = db.Column(db.String(10), nullable=False)  # Field for blood type
+    contact_info = db.Column(db.String(10), unique=True, nullable=False)  # Field for contact info
 
     def __repr__(self):
         return f'<User {self.username}>'
